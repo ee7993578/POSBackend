c96b4b310fc6fd2aabc46f82ef13b4ab8a7d2abc
ee7993578 <ee7993578@gmail.com>
2026-03-14 18:10:58 +0530

    Add new allowed origin to SecurityConfig

--- src/main/java/com/zosh/configrations/SecurityConfig.java
+++ src/main/java/com/zosh/configrations/SecurityConfig.java
@@ -55,7 +55,8 @@ public class SecurityConfig {
 						"http://localhost:3000",
 						"http://localhost:5173",
 						"https://zosh-pos.vercel.app",
-						"https://pos-sytem-bcs6.vercel.app"
+						"https://pos-sytem-bcs6.vercel.app",
+					     "https://pos-five-weld.vercel.app" 
 				));
 				cfg.setAllowedMethods(Collections.singletonList("*"));
 				cfg.setAllowCredentials(true);
